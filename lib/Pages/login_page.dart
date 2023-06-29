ed8ea2568d85c0d523712a64cd7f56be452a4dfb
AkritiSubedi <aakusubedi075@gmail.com>
2023-06-29 18:55:25 +0545

    Day 5 completed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,10 +1,18 @@
-// ignore_for_file: prefer_const_constructors, file_names, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors, file_names, sort_child_properties_last, non_constant_identifier_names, unused_import, use_build_context_synchronously
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool ChangeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -19,8 +27,8 @@ class LoginPage extends StatelessWidget {
               height: 20,
             ),
             Text(
-              "Welcome",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              "Welcome $name",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
@@ -31,6 +39,10 @@ class LoginPage extends StatelessWidget {
                       hintText: "Enter Username",
                       labelText: "Username",
                     ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   TextFormField(
                     obscureText: true,
@@ -40,15 +52,41 @@ class LoginPage extends StatelessWidget {
                     ),
                   ),
                   SizedBox(
-                    height: 20,
+                    height: 40,
                   ),
-                  ElevatedButton(
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(),
-                    onPressed: () {
-                      print("Hello Akriti");
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        ChangeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
                     },
-                  )
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: ChangeButton ? 50 : 150,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: ChangeButton? Icon(Icons.done,color: Colors.white,) 
+                      : Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      decoration: BoxDecoration(
+                          color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(ChangeButton ? 50 : 8)),
+                    ),
+                  ),
+                  //ElevatedButton(
+                  //  child: Text("Login"),
+                  //  style: TextButton.styleFrom(minimumSize: Size(150, 50)),
+                  //  onPressed: () {
+                  //    Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //  },
+                  //)
                 ],
               ),
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Pages/home_page.dart';
 import 'package:flutter_application_1/Pages/login_page.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,14 +15,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/Loginpage",
+      initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => Homepage(),
-        "/login": (context) => LoginPage()
+        MyRoutes.homeRoute: (context) => Homepage(),
+        MyRoutes.loginRoute: (context) => LoginPage()
       },
     );
   }
