7cecb3ce4b2ec67c08f009fbbf3385d0536e7de3
AkritiSubedi <aakusubedi075@gmail.com>
2023-07-09 16:07:07 +0545

    flutter-day-12 completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
-
+import 'package:flutter_application_1/models/catalog.dart';
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -14,14 +15,20 @@ class Homepage extends StatefulWidget {
 class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(6, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalogs App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text(context.runtimeType.toString()),
-        ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+            itemCount: dummyList.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index],
+              );
+            }),
       ),
       drawer: MyDrawer(),
     );
